5f4dcb129726c1a36dd592a4111ff0ad458dd14a
Komal-Solanki18 <komal.solanki_cs19@gla.ac.in>
2020-08-30 01:13:04 +0530

    create getters and setters methods for Student.java.

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -6,6 +6,8 @@
  * */
 package definitions;
 
+import java.util.Arrays;
+
 public class Student {
     private String firstName;
     private String middleName;
@@ -13,4 +15,88 @@ public class Student {
     private long universityRollNumber;
     private int numberOfBooksIssued;
     private Book[] namesOfTheBooksIssued;
+
+
+    /**
+     * This method returns the first name of the student.
+     *
+     * @return The first name of the student.
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * This method returns the middle name of the student.
+     *
+     * @return The middle name of the student.
+     */
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    /**
+     * This method returns the last name of the student.
+     *
+     * @return The last name of the student.
+     */
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * This method returns the university roll number of the student.
+     *
+     * @return The university roll number of the student.
+     */
+
+    public long getUniversityRollNumber() {
+        return universityRollNumber;
+    }
+
+    public void setUniversityRollNumber(long universityRollNumber) {
+        this.universityRollNumber = universityRollNumber;
+    }
+
+    /**
+     * This method returns the number of books issued by  the student.
+     *
+     * @return The number of books issued by  the student.
+     */
+
+    public int getNumberOfBooksIssued() {
+        return numberOfBooksIssued;
+    }
+
+    public void setNumberOfBooksIssued(int numberOfBooksIssued) {
+        this.numberOfBooksIssued = numberOfBooksIssued;
+    }
+
+    /**
+     * This method returns the names of the books issued by the student.
+     *
+     * @return The names of the books issued by the student
+     */
+
+    public Book[] getNamesOfTheBooksIssued() {
+        return namesOfTheBooksIssued;
+    }
+
+    public void setNamesOfTheBooksIssued(Book[] namesOfTheBooksIssued) {
+        this.namesOfTheBooksIssued = namesOfTheBooksIssued;
+    }
 }
